23b1c752ab206b3a2488cf9bc6c60ff3707145a5
Iqbalaulia <miqbalar12@gmail.com>
2020-09-05 20:35:43 +0700

    slipt to component

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center">
+            <p class="pt-4 pb-2">
+              2020 Copyright Store All Right Reserved Iqbal.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
