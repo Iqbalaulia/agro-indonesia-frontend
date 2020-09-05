5619227dffd8f90c2ddbbf56886af0a44371a769
Iqbalaulia <miqbalar12@gmail.com>
2020-09-05 20:35:32 +0700

    split to component

---
+++ src/components/Carousel.vue
@@ -0,0 +1,158 @@
+<template>
+    <div>
+      <section class="store-carousel">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8" data-aos="zoom-in">
+              <div
+                id="storeCarousel"
+                class="carousel slide"
+                data-ride="carousel"
+              >
+                <ol class="carousel-indicators">
+                  <li
+                    class="active"
+                    data-target="#storeCarousel"
+                    data-slide-to="0"
+                  ></li>
+                  <li
+                    class=""
+                    data-target="#storeCarousel"
+                    data-slide-to="1"
+                  ></li>
+                  <li
+                    class=""
+                    data-target="#storeCarousel"
+                    data-slide-to="2"
+                  ></li>
+                </ol>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img
+                      src="/images/carousel-0.jpg"
+                      class="d-block w-100"
+                      alt="Carousel Image"
+                    />
+                  </div>
+                  <div class="carousel-item">
+                    <img
+                      src="/images/carousel-1.jpg"
+                      class="d-block w-100"
+                      alt="Carousel Image"
+                    />
+                  </div>
+                  <div class="carousel-item">
+                    <img
+                      src="/images/carousel-2.jpg"
+                      class="d-block w-100"
+                      alt="Carousel Image"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 d-none d-sm-block" data-aos="zoom-in">
+              <div
+                id="storeCarousel"
+                class="carousel slide"
+                data-ride="carousel"
+              >
+                <ol class="carousel-indicators">
+                  <li
+                    class="active"
+                    data-target="#storeCarousel"
+                    data-slide-to="0"
+                  ></li>
+                  <li
+                    class=""
+                    data-target="#storeCarousel"
+                    data-slide-to="1"
+                  ></li>
+                  <li
+                    class=""
+                    data-target="#storeCarousel"
+                    data-slide-to="2"
+                  ></li>
+                </ol>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img
+                      src="/images/carousel-0.jpg"
+                      class="d-block w-100"
+                      alt="Carousel Image"
+                    />
+                  </div>
+                  <div class="carousel-item">
+                    <img
+                      src="/images/carousel-1.jpg"
+                      class="d-block w-100"
+                      alt="Carousel Image"
+                    />
+                  </div>
+                  <div class="carousel-item">
+                    <img
+                      src="/images/carousel-2.jpg"
+                      class="d-block w-100"
+                      alt="Carousel Image"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div
+                id="storeCarousel"
+                class="carousel slide mt-2"
+                data-ride="carousel"
+              >
+                <ol class="carousel-indicators">
+                  <li
+                    class="active"
+                    data-target="#storeCarousel"
+                    data-slide-to="0"
+                  ></li>
+                  <li
+                    class=""
+                    data-target="#storeCarousel"
+                    data-slide-to="1"
+                  ></li>
+                  <li
+                    class=""
+                    data-target="#storeCarousel"
+                    data-slide-to="2"
+                  ></li>
+                </ol>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img
+                      src="/images/carousel-0.jpg"
+                      class="d-block w-100"
+                      alt="Carousel Image"
+                    />
+                  </div>
+                  <div class="carousel-item">
+                    <img
+                      src="/images/carousel-1.jpg"
+                      class="d-block w-100"
+                      alt="Carousel Image"
+                    />
+                  </div>
+                  <div class="carousel-item">
+                    <img
+                      src="/images/carousel-2.jpg"
+                      class="d-block w-100"
+                      alt="Carousel Image"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Carousel'
+}
+</script>
---
+++ src/components/NewProduct.vue
@@ -0,0 +1,151 @@
+<template>
+  <div>
+    <section class="store-new-products">
+      <div class="container">
+        <div class="row">
+          <div class="col-12" data-aos="fade-up">
+            <h5>New Products</h5>
+          </div>
+        </div>
+        <div class="row">
+          <div
+            class="col-6 col-md-4 col-lg-3"
+            data-aos="fade-up"
+            data-aos-delay="100"
+          >
+            <a href="/details.html" class="component-products d-block">
+              <div class="products-thumbnail">
+                <div
+                  class="products-image"
+                  style="background-image: url('/images/product/karet.jpg')"
+                ></div>
+              </div>
+              <div class="products-text">Kebun Karet</div>
+              <div class="products-price">Rp.100000</div>
+            </a>
+          </div>
+          <div
+            class="col-6 col-md-4 col-lg-3"
+            data-aos="fade-up"
+            data-aos-delay="200"
+          >
+            <a href="/details.html" class="component-products d-block">
+              <div class="products-thumbnail">
+                <div
+                  class="products-image"
+                  style="background-image: url('/images/product/kelapa.jpg')"
+                ></div>
+              </div>
+              <div class="products-text">Kebun Kelapa</div>
+              <div class="products-price">Rp.100000</div>
+            </a>
+          </div>
+          <div
+            class="col-6 col-md-4 col-lg-3"
+            data-aos="fade-up"
+            data-aos-delay="300"
+          >
+            <a href="/details.html" class="component-products d-block">
+              <div class="products-thumbnail">
+                <div
+                  class="products-image"
+                  style="background-image: url('/images/product/kopi.jpg')"
+                ></div>
+              </div>
+              <div class="products-text">Kebun Kopi</div>
+              <div class="products-price">Rp.100000</div>
+            </a>
+          </div>
+          <div
+            class="col-6 col-md-4 col-lg-3"
+            data-aos="fade-up"
+            data-aos-delay="400"
+          >
+            <a href="/details.html" class="component-products d-block">
+              <div class="products-thumbnail">
+                <div
+                  class="products-image"
+                  style="background-image: url('/images/product/palm_1.jpg')"
+                ></div>
+              </div>
+              <div class="products-text">Kebun Kelapa Sawit</div>
+              <div class="products-price">Rp.100000</div>
+            </a>
+          </div>
+          <div
+            class="col-6 col-md-4 col-lg-3"
+            data-aos="fade-up"
+            data-aos-delay="500"
+          >
+            <a href="/details.html" class="component-products d-block">
+              <div class="products-thumbnail">
+                <div
+                  class="products-image"
+                  style="background-image: url('/images/product/susu.jpg')"
+                ></div>
+              </div>
+              <div class="products-text">Susu Sapi</div>
+              <div class="products-price">Rp.100000</div>
+            </a>
+          </div>
+          <div
+            class="col-6 col-md-4 col-lg-3"
+            data-aos="fade-up"
+            data-aos-delay="600"
+          >
+            <a href="/details.html" class="component-products d-block">
+              <div class="products-thumbnail">
+                <div
+                  class="products-image"
+                  style="background-image: url('/images/product/tea_1.jpg')"
+                ></div>
+              </div>
+              <div class="products-text">Kebun Teh</div>
+              <div class="products-price">Rp.100000</div>
+            </a>
+          </div>
+          <div
+            class="col-6 col-md-4 col-lg-3"
+            data-aos="fade-up"
+            data-aos-delay="700"
+          >
+            <a href="/details.html" class="component-products d-block">
+              <div class="products-thumbnail">
+                <div
+                  class="products-image"
+                  style="background-image: url('/images/product/tebu_1.jpg')"
+                ></div>
+              </div>
+              <div class="products-text">Kebun Tebu</div>
+              <div class="products-price">Rp.100000</div>
+            </a>
+          </div>
+          <div
+            class="col-6 col-md-4 col-lg-3"
+            data-aos="fade-up"
+            data-aos-delay="800"
+          >
+            <a href="/details.html" class="component-products d-block">
+              <div class="products-thumbnail">
+                <div
+                  class="products-image"
+                  style="
+                    background-image: url('/images/product/tembakau_1.jpg');
+                  "
+                ></div>
+              </div>
+              <div class="products-text">Kebun Tembakau</div>
+              <div class="products-price">Rp.100000</div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NewProduct",
+};
+</script>
